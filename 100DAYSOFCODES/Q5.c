5fc98dccace7aa8f0a5aad1b4ccd3f705c04798b
shirish983 <shirishk819@gmail.com>
2025-10-08 00:05:05 +0530

    Create Q5.c

---
+++ 100DAYSOFCODES/Q5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    float celsius = 60.0;
+    float fahrenheit = (celsius*9/5)+32;
+    printf("temperature in celsius is =%.2f\n",celsius);
+    printf("temperature in fahrenheit=%.2f\n",fahrenheit);
+    return 0;
+}
