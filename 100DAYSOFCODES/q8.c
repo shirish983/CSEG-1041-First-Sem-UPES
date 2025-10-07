ceda1c7ec6d068bbe9f3174725eca855b79769ea
shirish983 <shirishk819@gmail.com>
2025-10-08 00:06:32 +0530

    Create q8.c

---
+++ 100DAYSOFCODES/q8.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a = 80;
+    int sum =0;
+    for(int i =1;i<=a;i++){
+        sum += i;
+
+    }
+    printf("the sum of first %d natural  number =%d\n", a , sum);
+    return 0;
+    
+}
