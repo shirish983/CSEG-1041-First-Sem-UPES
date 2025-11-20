ef7d922d4a365457e20d3e5054732e0d5173cf42
shirish983 <shirishk819@gmail.com>
2025-11-20 19:31:49 +0530

    Create q62.c

---
+++ 100DAYSOFCODES/q62.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int start = 0, end = n - 1;
+
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
