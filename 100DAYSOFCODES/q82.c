c6ab1654d91f486e791622e2711bd6a1d591ac45
shirish983 <shirishk819@gmail.com>
2025-11-21 00:59:01 +0530

    Create q82.c

---
+++ 100DAYSOFCODES/q82.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    int i = 0;
+    fgets(str, sizeof(str), stdin);
+    while(str[i] != '\0' && str[i] != '\n') {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    return 0;
+}
