7ef9aef4b7d34c4b7bc44c7f1749ac962b473a64
shirish983 <shirishk819@gmail.com>
2025-11-20 19:15:21 +0530

    Add program to calculate sum of a series

---
+++ 100DAYSOFCODES/q45.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    double sum = 0.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Enter a positive number.\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        double num = 2 * i;
+        double den = 4 * i - 1;
+        sum += num / den;
+    }
+
+    printf("Sum of the series up to %d terms = %.6f\n", n, sum);
+
+    return 0;
+}
