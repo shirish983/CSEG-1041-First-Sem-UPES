0b15fc65df8bcef0d77ecef5eae1b7a0ba019fc8
shirish983 <shirishk819@gmail.com>
2025-11-20 18:54:01 +0530

    Add basic calculator program in q25.c

---
+++ 100DAYSOFCODES/q25.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);   // space before %c to ignore leftover newline
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch (op) {
+        case '+':
+            printf("Result = %d\n", a + b);
+            break;
+
+        case '-':
+            printf("Result = %d\n", a - b);
+            break;
+
+        case '*':
+            printf("Result = %d\n", a * b);
+            break;
+
+        case '/':
+            if (b == 0)
+                printf("Error: Division by zero.\n");
+            else
+                printf("Result = %d\n", a / b);
+            break;
+
+        case '%':
+            if (b == 0)
+                printf("Error: Modulo by zero.\n");
+            else
+                printf("Result = %d\n", a % b);
+            break;
+
+        default:
+            printf("Invalid operator.\n");
+    }
+
+    return 0;
+}
