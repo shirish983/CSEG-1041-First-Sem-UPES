337a584d29f18da2b02479bf7da0c2621b0ce386
shirish983 <shirishk819@gmail.com>
2025-11-20 19:12:22 +0530

    Create q42.c

---
+++ 100DAYSOFCODES/q42.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Enter a positive number.\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == n)
+        printf("%d is a perfect number.\n", n);
+    else
+        printf("%d is not a perfect number.\n", n);
+
+    return 0;
+}
