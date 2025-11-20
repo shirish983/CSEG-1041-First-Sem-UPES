fedbe3d2a8a068ee3c322e03711a8d3e5e7545d8
shirish983 <shirishk819@gmail.com>
2025-11-20 19:24:41 +0530

    Create q53.c

---
+++ 100DAYSOFCODES/q53.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Upper half
+    for (i = 1; i <= 9; i += 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = 7; i >= 1; i -= 2) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
