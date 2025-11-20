101e21f5e0d0cacf4f33424130e4e61b23386844
shirish983 <shirishk819@gmail.com>
2025-11-20 19:25:26 +0530

    Add diamond pattern printing program in q54.c
    Implement a program to print a diamond pattern using asterisks.

---
+++ 100DAYSOFCODES/q54.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    // Upper half
+    for (i = 1; i <= 7; i += 2) {
+        space = (7 - i) / 2;
+        for (j = 1; j <= space; j++)
+            printf(" ");
+        for (j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    // Lower half
+    for (i = 5; i >= 1; i -= 2) {
+        space = (7 - i) / 2;
+        for (j = 1; j <= space; j++)
+            printf(" ");
+        for (j = 1; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
