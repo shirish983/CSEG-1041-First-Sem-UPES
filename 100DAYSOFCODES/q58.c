86cb7e3ef6cad0717aecfe5b5a013a11966578e9
shirish983 <shirishk819@gmail.com>
2025-11-20 19:28:44 +0530

    Create q58.c

---
+++ 100DAYSOFCODES/q58.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+
+    return 0;
+}
