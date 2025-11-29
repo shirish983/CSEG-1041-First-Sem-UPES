85bcb92f78a3b11aaf16bba2c69336f2bfbc145d
shirish983 <shirishk819@gmail.com>
2025-11-29 23:44:41 +0530

    Create q135.c

---
+++ 100DAYSOFCODES/q135.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+enum Number { A=10, B, C, D };
+
+int main() {
+    printf("%d\n", A);
+    printf("%d\n", B);
+    printf("%d\n", C);
+    printf("%d\n", D);
+    return 0;
+}
