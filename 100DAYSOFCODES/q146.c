0fefc80745ef95ccf12c024f832a6d28924c1ff3
shirish983 <shirishk819@gmail.com>
2025-11-29 23:52:25 +0530

    Create q146.c

---
+++ 100DAYSOFCODES/q146.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Date {
+    int day;
+    int month;
+    int year;
+};
+
+struct Employee {
+    char name[50];
+    int id;
+    struct Date joining;
+};
+
+int main() {
+    struct Employee e;
+    scanf("%s %d %d %d %d", e.name, &e.id, &e.joining.day, &e.joining.month, &e.joining.year);
+    printf("Name: %s\nID: %d\nJoining Date: %02d/%02d/%04d\n", e.name, e.id, e.joining.day, e.joining.month, e.joining.year);
+    return 0;
+}
