b76faf9f8e9ff3ebbc558eddbb1e0584311ed29d
shirish983 <shirishk819@gmail.com>
2025-11-29 23:36:08 +0530

    Create q128.c

---
+++ 100DAYSOFCODES/q128.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char ch;
+    int vowels = 0, consonants = 0;
+
+    fp = fopen("info.txt", "r");
+    if (fp == NULL) {
+        printf("File not found\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        ch = tolower(ch);
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
